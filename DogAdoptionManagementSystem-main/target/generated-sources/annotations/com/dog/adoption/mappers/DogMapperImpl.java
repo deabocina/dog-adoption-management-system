8010762590a4fa7cd627dca55ba48ee6dbeb3f81
Dea Bočina <140092973+deabocina@users.noreply.github.com>
2024-02-14 13:23:11 +0100

    Add files via upload

---
+++ DogAdoptionManagementSystem-main/target/generated-sources/annotations/com/dog/adoption/mappers/DogMapperImpl.java
@@ -0,0 +1,13 @@
+package com.dog.adoption.mappers;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-02-14T12:54:31+0100",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 21.0.1 (Oracle Corporation)"
+)
+@Component
+public class DogMapperImpl extends DogMapper {
+}
